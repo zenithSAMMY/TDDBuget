3dfba8b7bf40b819f5be5718568ed4d35fb06a21
Zenith Yang <z2006005@gmail.com>
2022-10-08 16:55:57 +0800

    complete

---
+++ TDDBuget/TDDBuget/BudgetService.cs
@@ -0,0 +1,65 @@
+﻿namespace TDDBuget;
+
+public class BudgetService
+{
+    private readonly IBudgetRepo _repo;
+    private readonly Budget defaultBudget;
+    
+    public BudgetService(IBudgetRepo repo)
+    {
+        _repo = repo;
+        defaultBudget = new Budget()
+        {
+            YearMonth = "",
+            Amount = 0
+        };
+    }
+    
+    public decimal Query(DateTime start, DateTime end)
+    {
+        if (start > end)
+        {
+            return 0;
+        }
+        var budgetData =  _repo.GetAll();
+
+        if (start.Month == end.Month)
+        {
+            if (start.Day == 1 && (end.AddDays(1).Month - start.Month) == 1)
+            {
+                var result = budgetData.Where(x => x.YearMonth == start.ToString("yyyyMM")).FirstOrDefault(defaultBudget);
+
+                return result.Amount;
+            }
+            else
+            {
+                var result = budgetData.Where(x => x.YearMonth == start.ToString("yyyyMM")).FirstOrDefault(defaultBudget);
+
+                var days = (end - start).Days + 1;
+                var total = DateTime.DaysInMonth(start.Year, start.Month);
+                return result.Amount / total * days;
+            }
+        }
+
+        var endTime = new DateTime(end.Year,end.Month,01);
+        var queryList = new List<string>();
+        for (var currentTime = start.AddMonths(1); currentTime < endTime; currentTime = currentTime.AddMonths(1))
+        {
+            queryList.Add(currentTime.ToString("yyyyMM"));
+        }
+
+        var budgets = budgetData.Where(w => queryList.Contains(w.YearMonth));
+        var sum = budgets.Sum(s => s.Amount);
+
+        var startBudget = budgetData.Where(x => x.YearMonth == start.ToString("yyyyMM")).FirstOrDefault(defaultBudget);
+        var endBudget = budgetData.Where(x => x.YearMonth == end.ToString("yyyyMM")).FirstOrDefault(defaultBudget);
+        var startMonthDays = DateTime.DaysInMonth(start.Year, start.Month);
+        var endMonthDays = DateTime.DaysInMonth(end.Year, end.Month);
+        var startDays = startMonthDays - start.Day + 1;
+        var endDays = end.Day;
+
+        return (startBudget.Amount / startMonthDays * startDays) + (endBudget.Amount / endMonthDays * endDays) + sum;
+
+        return 0;
+    }
+}
---
+++ TDDBuget/TDDBuget/IBugetRepo.cs
@@ -0,0 +1,12 @@
+﻿namespace TDDBuget;
+
+public interface IBudgetRepo
+{
+    public List<Budget> GetAll();
+}
+
+public class Budget
+{
+    public string YearMonth { get; set; }
+    public int Amount { get; set; }
+}
